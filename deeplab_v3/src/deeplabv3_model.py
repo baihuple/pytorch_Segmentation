9386f18bc9da5b4543c6f51455e87cb0bb828696
baihuple <90971963+baihuple@users.noreply.github.com>
2023-11-28 09:52:06 +0800

    Add files via upload

---
+++ deeplab_v3/src/deeplabv3_model.py
@@ -0,0 +1,306 @@
+from collections import OrderedDict
+
+from typing import Dict, List
+
+import torch
+from torch import nn, Tensor
+from torch.nn import functional as F
+from .resnet_backbone import resnet50, resnet101
+from .mobilenet_backbone import mobilenet_v3_large
+
+''' 网络构造方法 '''
+''' 1、简单、使用次数少、参数固定 Conv: 不用构造类，直接 nn.Sequential() [ 运行按顺序运行 ] '''
+''' 2、稍微复杂、使用次数多、需要不同参数 Conv: 构造类，继承 nn.Sequential() --- 若为纯 Conv\BN\RELU 不用定义 forward ; 
+否则定义 forward 及返回值 '[ 没有 forward 按照顺序运行，有 forward 按照 forward 运行 ] '''
+''' 3、结构更复杂: 继承 nn.Module() 类,定义相关网络结构,同时定义 forward 函数'''
+
+class IntermediateLayerGetter(nn.ModuleDict):
+    """
+    Module wrapper that returns intermediate layers from a model
+
+    It has a strong assumption that the modules have been registered
+    into the model in the same order as they are used.
+    This means that one should **not** reuse the same nn.Module
+    twice in the forward if you want this to work.
+
+    Additionally, it is only able to query submodules that are directly
+    assigned to the model. So if `model` is passed, `model.feature1` can
+    be returned, but not `model.feature1.layer2`.
+
+    Args:
+        model (nn.Module): model on which we will extract the features
+        return_layers (Dict[name, new_name]): a dict containing the names
+            of the modules for which the activations will be returned as
+            the key of the dict, and the value of the dict is the name
+            of the returned activation (which the user can specify).
+    """
+    _version = 2
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
+
+    def __init__(self, model: nn.Module, return_layers: Dict[str, str]) -> None:
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
+
+        # 重新构建backbone，将没有使用到的模块全部删掉
+        layers = OrderedDict()
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
+
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        out = OrderedDict()
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
+
+
+class DeepLabV3(nn.Module):
+    """
+    Implements DeepLabV3 model from
+    `"Rethinking Atrous Convolution for Semantic Image Segmentation"
+    <https://arxiv.org/abs/1706.05587>`_.
+
+    Args:
+        backbone (nn.Module): the network used to compute the features for the model.
+            The backbone should return an OrderedDict[Tensor], with the key being
+            "out" for the last feature map used, and "aux" if an auxiliary classifier
+            is used.
+        classifier (nn.Module): module that takes the "out" element returned from
+            the backbone and returns a dense prediction.
+        aux_classifier (nn.Module, optional): auxiliary classifier used during training
+    """
+    __constants__ = ['aux_classifier']
+
+    def __init__(self, backbone, classifier, aux_classifier=None):
+        super(DeepLabV3, self).__init__()
+        self.backbone = backbone
+        self.classifier = classifier
+        self.aux_classifier = aux_classifier
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        input_shape = x.shape[-2:]
+        # contract: features is a dict of tensors
+        # Step1：backbone
+        features = self.backbone(x)
+
+        # Step2.1：主分支输出
+        result = OrderedDict()
+        x = features["out"]
+        x = self.classifier(x)
+        # 使用双线性插值还原回原图尺度
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+        result["out"] = x
+
+        # Step2.2: 辅助分支输出
+        if self.aux_classifier is not None:
+            x = features["aux"]
+            x = self.aux_classifier(x)
+            # 使用双线性插值还原回原图尺度
+            x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+            result["aux"] = x
+
+        return result
+
+
+class FCNHead(nn.Sequential):
+    def __init__(self, in_channels, channels):
+        inter_channels = in_channels // 4
+        super(FCNHead, self).__init__(
+            nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(inter_channels, channels, 1)
+        )
+
+''' 膨胀卷积层 '''
+# dilation - 膨胀系数
+class ASPPConv(nn.Sequential):
+    def __init__(self, in_channels: int, out_channels: int, dilation: int) -> None:
+        super(ASPPConv, self).__init__(
+            nn.Conv2d(in_channels, out_channels, 3, padding=dilation, dilation=dilation, bias=False),   # 膨胀卷积结构如图示 padding = dialation
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+''' 池化: input -->  '''
+class ASPPPooling(nn.Sequential):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(ASPPPooling, self).__init__(
+            nn.AdaptiveAvgPool2d(1),                                # 1)全局池化 - 输出为 1*1
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),    # 2)Conv1*1 - 调节channel
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        size = x.shape[-2:]     # 获取 size
+        for mod in self:        # 获取每个模块，并带入
+            x = mod(x)
+        # F.interpolate() 双线性插值上采样，恢复 size 到输入 tensor 的size
+        return F.interpolate(x, size=size, mode='bilinear', align_corners=False)
+
+''' ASPP 类 '''
+class ASPP(nn.Module):
+    # atrous_rates - 膨胀分支采用的膨胀系数列表
+    def __init__(self, in_channels: int, atrous_rates: List[int], out_channels: int = 256) -> None:
+        super(ASPP, self).__init__()
+
+        ''' 添加 ASPP 中的 modules 模块 '''
+        modules = [
+            # M1. Conv1*1
+            nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                          nn.BatchNorm2d(out_channels),
+                          nn.ReLU())
+        ]
+
+        # M2\3\4. 添加 3 个膨胀卷积结构 ASPPConv
+        rates = tuple(atrous_rates)
+        for rate in rates:
+            modules.append(ASPPConv(in_channels, out_channels, rate))
+
+        # M5. ASPP-pooling
+        modules.append(ASPPPooling(in_channels, out_channels))
+
+        # nn.ModuleList() 构建 ASPP 中的 5 个分支< 结合 M1\2\3\4\5 >
+        self.convs = nn.ModuleList(modules)
+
+        # ASPP concat 后的 Conv1*1
+        # nn.Sequential() 用于简单的顺序连接模型
+        self.project = nn.Sequential(
+            # 深度方向拼接
+            nn.Conv2d(len(self.convs) * out_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _res = []
+        # 分别遍历 ASPP 的 5 个分支
+        for conv in self.convs:
+            _res.append(conv(x))
+        # 将 5 个分支在 channel 维度上进行拼接
+        res = torch.cat(_res, dim=1)
+        # 最终返回 ASPP 结构的输出
+        return self.project(res)
+
+''' 构建主分支的 DeepLab Head - 包含 ASPP 结构等 '''
+class DeepLabHead(nn.Sequential):
+    def __init__(self, in_channels: int, num_classes: int) -> None:
+        super(DeepLabHead, self).__init__(
+            ASPP(in_channels, [12, 24, 36]),                # ASPP 结构 < 中间 3*3 卷积的膨胀系数：12、24、36 >
+            nn.Conv2d(256, 256, 3, padding=1, bias=False),  # 
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, num_classes, 1)
+        )
+
+''' 创建 deeplabv3_resnet50  '''
+def deeplabv3_resnet50(aux, num_classes=21, pretrain_backbone=False):
+    # 'resnet50_imagenet': 'https://download.pytorch.org/models/resnet50-0676ba61.pth'
+    # 'deeplabv3_resnet50_coco': 'https://download.pytorch.org/models/deeplabv3_resnet50_coco-cd0a2569.pth'
+    backbone = resnet50(replace_stride_with_dilation=[False, True, True])
+
+    if pretrain_backbone:
+        # 载入resnet50 backbone预训练权重
+        backbone.load_state_dict(torch.load("resnet50.pth", map_location='cpu'))
+
+    out_inplanes = 2048
+    aux_inplanes = 1024
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    # why using aux: https://github.com/pytorch/vision/issues/4292
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    # out_inplanes - backbone 输出特征层的 channel
+    # num_classes - 分类类别数
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabV3(backbone, classifier, aux_classifier)
+
+    return model
+
+
+def deeplabv3_resnet101(aux, num_classes=21, pretrain_backbone=False):
+    # 'resnet101_imagenet': 'https://download.pytorch.org/models/resnet101-63fe2227.pth'
+    # 'deeplabv3_resnet101_coco': 'https://download.pytorch.org/models/deeplabv3_resnet101_coco-586e9e4e.pth'
+    backbone = resnet101(replace_stride_with_dilation=[False, True, True])
+
+    if pretrain_backbone:
+        # 载入resnet101 backbone预训练权重
+        backbone.load_state_dict(torch.load("resnet101.pth", map_location='cpu'))
+
+    out_inplanes = 2048
+    aux_inplanes = 1024
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    # why using aux: https://github.com/pytorch/vision/issues/4292
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabV3(backbone, classifier, aux_classifier)
+
+    return model
+
+
+def deeplabv3_mobilenetv3_large(aux, num_classes=21, pretrain_backbone=False):
+    # 'mobilenetv3_large_imagenet': 'https://download.pytorch.org/models/mobilenet_v3_large-8738ca79.pth'
+    # 'depv3_mobilenetv3_large_coco': "https://download.pytorch.org/models/deeplabv3_mobilenet_v3_large-fc3c493d.pth"
+    backbone = mobilenet_v3_large(dilated=True)
+
+    if pretrain_backbone:
+        # 载入mobilenetv3 large backbone预训练权重
+        backbone.load_state_dict(torch.load("mobilenet_v3_large.pth", map_location='cpu'))
+
+    backbone = backbone.features
+
+    # Gather the indices of blocks which are strided. These are the locations of C1, ..., Cn-1 blocks.
+    # The first and last blocks are always included because they are the C0 (conv1) and Cn.
+    stage_indices = [0] + [i for i, b in enumerate(backbone) if getattr(b, "is_strided", False)] + [len(backbone) - 1]
+    out_pos = stage_indices[-1]  # use C5 which has output_stride = 16
+    out_inplanes = backbone[out_pos].out_channels
+    aux_pos = stage_indices[-4]  # use C2 here which has output_stride = 8
+    aux_inplanes = backbone[aux_pos].out_channels
+    return_layers = {str(out_pos): "out"}
+    if aux:
+        return_layers[str(aux_pos)] = "aux"
+
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    # why using aux: https://github.com/pytorch/vision/issues/4292
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabV3(backbone, classifier, aux_classifier)
+
+    return model
